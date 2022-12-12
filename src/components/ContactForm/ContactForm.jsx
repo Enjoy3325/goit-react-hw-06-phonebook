54c37479a0d237082dc2d29f72deab0db9a20ec2
Enjoy3325 <enjoy3325@gmail.com>
2022-12-12 15:12:15 +0200

    add placeholder

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -55,6 +55,7 @@ export const ContactForm = () => {
             type="text"
             name="name"
             value={name}
+            placeholder="Nick"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
@@ -68,6 +69,7 @@ export const ContactForm = () => {
             type="tel"
             name="number"
             value={number}
+            placeholder="+38(063)01-001-01"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
