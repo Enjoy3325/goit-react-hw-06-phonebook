258fc29ce3e841fee3c85dfb22e295023a0c9eea
Enjoy3325 <enjoy3325@gmail.com>
2022-12-12 11:49:50 +0200

    видалив минулу закоменчену логіку з App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,52 +8,6 @@ import { ContactForm } from '../ContactForm/ContactForm';
 import { Wrapper } from './App.styled';
 
 export const App = () => {
-  // const contact = useSelector(state => state.contact);
-  // const filterValueSelector = useSelector(state => state.contacts.filter);
-
-  // const contacts = localStorage.getItem('contacts');
-  // const parseContacts = JSON.parse(contacts);
-  // if (parseContacts) {
-  //   setContacts(parseContacts);
-  // }
-
-  // function Fares(fsf) {}
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const getNewContact = dataValue => {
-  //   if (checkContacts(dataValue.name)) {
-  //     return alert(`${dataValue.name} is already in contacts`);
-  //   }
-  //   setContacts(prevState => [...prevState, { id: nanoid(), ...dataValue }]);
-  // };
-
-  // const checkContacts = e => {
-  //   return contacts.find(
-  //     contact => contact.name.toLowerCase() === e.toLowerCase()
-  //   );
-  // };
-
-  // const ChangeFilter = e => {
-  //   dispatch(chengeFilter(e.target.value));
-  // };
-
-  // const handlDeleteContacts = id => {
-  //   setContacts(
-  //     contacts.filter(contact => {
-  //       return contact.id !== id;
-  //     })
-  //   );
-  // };
-
-  // const filterContact = () => {
-  //   const filterNormalized = filterValueSelector.toLowerCase();
-  //   return contacts.filter(contact => {
-  //     return contact.name.toLowerCase().includes(filterNormalized);
-  //   });
-  // };
-
   return (
     <Wrapper>
       <div>
